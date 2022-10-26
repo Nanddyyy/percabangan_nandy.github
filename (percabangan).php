20e9a8c8beed575d736255039b9ed5f9c94436b7
Nanddyyy <116326229+Nanddyyy@users.noreply.github.com>
2022-10-26 20:13:57 +0700

    Update (percabangan).php

--- (percabangan).php
+++ (percabangan).php
@@ -1,3 +1,17 @@
-# percabangan_nandy.github
-Ngzhwvgw
-(percabangan).php
+<?php
+$a = 444;
+$b = 58;
+
+if($a > $b)
+{
+echo "$a lebih besar dari $b";
+}
+elseif($a < $b)
+{
+echo "$a lebih kecil dari $b";
+}
+elseif($a == $b)
+{
+echo "$a sama dengan $b";
+}
+?>
